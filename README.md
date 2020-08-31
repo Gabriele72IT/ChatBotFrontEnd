0dce0fbaed04578152db0176d65f11075f8a4c13
Guilherme Alves <guii05@hotmail.com>
2020-08-30 23:50:53 -0300

    Simulador de comportament do chat

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # ChatBotFrontEnd
 
-
-Teste 
+Estes projeto é um client que consome a api: https://github.com/guialiveira/ChatBotApi
+e simula a programação de um chat automatizado para atendimento genérico.
+<br/>
+<br/>
+Lista etapas cadastradas:
+<br/>
+<img src="prints/GetAll.PNG">
+<br/><br/>
+Cadastro de frase e respostas para dialogo:
+<br/>
+<img src="prints/Cadastro.PNG">
+<br/><br/>
+Simulandor de comportamento do chat:
+<br/><br/>
+<img src="prints/ChatRodando.PNG"><br/><br/>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ function Cadastrar() {
 	var obj;
 	var _texto = document.querySelector('#texto').value;
 	var _tipo = $('input[name=tipo]:checked').val();
-	if(_tipo === "0"){
+	if (_tipo === "0") {
 
 		var respostaText1 = document.querySelector('#respostaText1').value;
 		var proxEtapaResposta1 = document.querySelector('#proxEtapaResposta1').value;
@@ -20,32 +20,33 @@ function Cadastrar() {
 
 		var _resposta2 = {
 			legenda: respostaText2,
-			proxEtapaId: proxEtapaResposta2			
+			proxEtapaId: proxEtapaResposta2
 		}
-	
+
 		obj = {
 			texto: _texto,
 			tipo: Number(_tipo),
-			respostas: [ 
+			respostas: [
 				_resposta1,
 				_resposta2],
 		}
-	}else{
+	} else {
 		var _proxEtapaId = document.querySelector('#proxEtapaId').value;
-		
+
 		obj = {
 			texto: _texto,
 			proxEtapaId: Number(_proxEtapaId),
 			tipo: Number(_tipo),
 			respostas: [],
 		}
-	}	
+	}
 	fazerPost(obj)
 	$('#myModal').modal('hide')
+
 }
 
 function carregaEtapas(metodo, id, corpo) {
-	tbody.innerHTML = '';
+	$("#tabelao").html("");
 
 	var xhr = new XMLHttpRequest();
 
@@ -64,12 +65,12 @@ function carregaEtapas(metodo, id, corpo) {
 carregaEtapas('GET');
 
 //Controle de Formulario respostas ou perguntas
-$('#r11').on('click', function(){
-  $(this).parent().find('a').trigger('click')
+$('#r11').on('click', function () {
+	$(this).parent().find('a').trigger('click')
 })
 
-$('#r12').on('click', function(){
-  $(this).parent().find('a').trigger('click')
+$('#r12').on('click', function () {
+	$(this).parent().find('a').trigger('click')
 })
 
 function fazerPost(corpo) {
@@ -79,32 +80,91 @@ function fazerPost(corpo) {
 	xhr.send(JSON.stringify(corpo));
 }
 
-function ediatar(id) {
-
-	console.log(id);
-}
 
 function imprimeEtapa(etapa) {
 
-	if(etapa.tipo === 0){
+	if (etapa.tipo === 0) {
 		var trow = `<tr>
 							<td>${etapa.texto}</td>
 							<td>${etapa.proxEtapaId}</td>
 							<td>${etapa.id}</td>							
 							<td>${etapa.respostas[0].legenda} | ${etapa.respostas[1].legenda}</td>
-							<td><button type="button" class="btn btn-warning" onclick='editarEstudante(${JSON.stringify(etapa)})'>Editar</button></td>
+							<td><button type="button" class="btn btn-danger" onclick='excluir(${JSON.stringify(etapa.id)})'>Excluir</button></td>
 					    </tr>
 					   `
-	tbody.innerHTML += trow;
-	}else{
+		$("#tabelao").append(trow);
+	} else {
 		var trow = `<tr>
 							<td>${etapa.texto}</td>
 							<td>${etapa.proxEtapaId}</td>
 							<td>${etapa.id}</td>							
-							<td>${etapa.respostas}</td>
-							<td><button type="button" class="btn btn-warning" onclick='editarEstudante(${JSON.stringify(etapa)})'>Editar</button></td>
+							<td> Não Preparadas</td>
+							<td><button type="button" class="btn btn-danger" onclick='excluir(${JSON.stringify(etapa.id)})'>Excluir</button></td>
 					    </tr>
 					   `
-	tbody.innerHTML += trow;
+		$("#tabelao").append(trow);
 	}
+}
+
+function excluir(id){
+	var xhr = new XMLHttpRequest();
+	xhr.open("DELETE", `https://localhost:44314/api/Etapas/${id}`, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(true);
+	Location.reload()
+}
+
+function imprimeNoChat(etapa) {
+	var trow
+	if (etapa.proxEtapaId != 0) {
+		trow = `<tr>
+					<td>${etapa.id}</td>
+					<td>${etapa.texto}</td>						
+				</tr>
+				`
+		$("#chat").append(trow);
+		carregaEtapa(etapa.proxEtapaId)
+	} else {
+		trow = `<tr>
+					<td>${etapa.id}</td>
+					<td>${etapa.texto}</td>						
+					<td><button type="button" class="btn btn-success" onclick='carregaEtapa(${etapa.respostas[0].proxEtapaId})'>${etapa.respostas[0].legenda} </button></td>
+					<td><button type="button" class="btn btn-danger" onclick='carregaEtapa(${etapa.respostas[1].proxEtapaId})'>${etapa.respostas[1].legenda} </button></td>
+				</tr>
+				 `
+		$("#chat").append(trow);
+	}
+}
+
+function carregaEtapa(id) {
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', `https://localhost:44314/api/Etapas/${id}`, true);
+
+	xhr.setRequestHeader('Content-Type', 'application/json');		
+	xhr.onreadystatechange = function (oEvent) {  
+		if (xhr.readyState === 4) {  
+			if (xhr.status === 200) {  
+
+				xhr.onload = function () {
+					var etapa = JSON.parse(this.responseText);
+					imprimeNoChat(etapa)
+				}
+
+			} else {  
+			   console.log("Error", xhr.statusText);  
+			}  
+		} 
+		//xhr.send(); 
+	}; 
+	
+	xhr.send(null);  
+
+}
+
+function chatOn(id) {
+	$("#chat").html("");
+	carregaEtapa(1)
+
 }
